d00eb6cbd2eed67f23d8f95cfdd84b5967fec93c
Brady Robshaw <heybrady88@gmail.com>
2023-06-09 15:22:32 -0400

    Initial Commit

---
+++ SetTracker/SetTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  SetTrackerApp.swift
+//  SetTracker
+//
+//  Created by Brady Robshaw on 6/9/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SetTrackerApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Item.self)
+    }
+}
