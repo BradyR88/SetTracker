89ebe189a30bbab99278937b2a6b90558476aa99
Brady Robshaw <heybrady88@gmail.com>
2023-06-14 20:58:19 -0400

    Add ZoneSummaryView and navigation to it

---
+++ SetTracker/Views/ZoneSummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  ZoneSummaryView.swift
+//  SetTracker
+//
+//  Created by Brady Robshaw on 6/14/23.
+//
+
+import SwiftUI
+
+struct ZoneSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ZoneSummaryView()
+}
